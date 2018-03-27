67ab214986622c1f302d337b34506a3de86f739f
David Chen <davidchenlee@gmail.com>
2018-03-27 13:20:52 +0200

    Add MatlabAPI folder

--- Playground/Playground/Main.cpp
+++ Playground/Playground/Main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include "TiffEx.h"
 #include "ConcurrencyEx.h"
+#include <Windows.h>	//ShellExecute
 using namespace std;
-
 
 int main()
 {
@@ -11,6 +11,10 @@ int main()
 	//WriteTiffEx();
 	ThreadEx();
 	getchar();
+
+	//"double click" on the specified file
+	ShellExecute(NULL, "open", "D:\\OwnCloud\\Codes\\Visual-Studio-tests\\MatlabAPI\\x64\\Release\\MatlabAPI.exe", NULL, NULL, SW_SHOWDEFAULT);
+
 	return 0;
 }
 
