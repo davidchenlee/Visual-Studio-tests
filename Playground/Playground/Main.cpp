67bb78e0072310b3a0e7ba12b28b44df82851447
David Chen <davidchenlee@gmail.com>
2018-11-22 06:17:28 +0100

    Commit Playground

--- Playground/Playground/Main.cpp
+++ Playground/Playground/Main.cpp
@@ -1,70 +1,99 @@
-/*
-Create a process to open a file in ImageJ
-CreateProcess example from: https://stackoverflow.com/questions/32926272/beginner-c-createprocess-error-2
-ImageJ command line options: https://imagej.nih.gov/ij/docs/install/windows.html
-
-*/
-
-#include <windows.h>
+#include <chrono>
 #include <iostream>
 #include <stdio.h>
-#include <tchar.h>
-//#include <string>
-using namespace std;
+#include <vector>
+
+class A
+{
+	const int mNpix;
+	std::vector<uint32_t> mVectorU32;
+	uint32_t* mVectorPointer;
+public:
+	A(const int nPix);
+	~A();
+	void test();
+	void test2();
+};
+
+A::A(const int nPix): mNpix(nPix), mVectorU32(nPix)
+{
+	mVectorPointer = new uint32_t[nPix]();
+}
+
+A::~A()
+{
+	delete[] mVectorPointer;
+}
+
+void A::test()
+{
+	//std::vector<unsigned char> vectorChar(mNpix);
+
+	for (int pixIndex = 0; pixIndex < mNpix; pixIndex++)
+	{
+		//mVectorU32[pixIndex];
+		mVectorPointer[pixIndex];
+		//vectorChar[pixIndex] = upscaled;
+	}
+}
+
+
+void A::test2()
+{
+	//std::vector<unsigned char> vectorChar(mNpix);
+
+	for (int pixIndex = 0; pixIndex < mNpix; pixIndex++)
+	{
+		mVectorU32[pixIndex];
+		//mVectorPointer[pixIndex];
+		//vectorChar[pixIndex] = upscaled;
+	}
+}
 
 
 void main()
 {
 
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
+	const int nPix = 300 * 400 * 10;
 
-	ZeroMemory(&si, sizeof(si));
-	ZeroMemory(&pi, sizeof(pi));
+	A a(nPix);
 
-	si.cb = sizeof(si);
+	//Declare and start a stopwatch
+	double duration;
+	auto t_start = std::chrono::high_resolution_clock::now();
+	a.test();
+	//Stop the stopwatch
+	duration = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - t_start).count();
 
-/*
-	if (argc != 2)
-	{
-		printf("Usage: %s [cmdline]\n", argv[0]);
-		return;
-	}
-	*/
 
-	//Run a macro: the first path is for the ImageJ API, the second path for the ImageJ macro
-	//LPSTR command = "java -jar \"C:\\Users\\chen\\Desktop\\ImageJ-win64\\jars\\ij-1.51w.jar\" -macro C:\\Users\\chen\\Desktop\\ImageJ-win64\\macros\\macroTest.ijm";
+	//Declare and start a stopwatch
+	double duration2;
+	t_start = std::chrono::high_resolution_clock::now();
+	a.test2();
+	//Stop the stopwatch
+	duration2 = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - t_start).count();
 
-	//Open a file: the first path is for the ImageJ API, the second path for the file
-	LPSTR command = "java -jar \"C:\\Users\\chen\\Desktop\\ImageJ-win64\\jars\\ij-1.51w.jar\" C:\\Users\\chen\\Desktop\\tiffExample.tif";
 
-	// Start the child process. 
-	if (!CreateProcess(NULL,   // No module name (use command line)
-		command,        // Command line
-		NULL,				// Process handle not inheritable
-		NULL,				// Thread handle not inheritable
-		FALSE,				// Set handle inheritance to FALSE
-		CREATE_NO_WINDOW,	// Creation flags
-		NULL,				// Use parent's environment block
-		NULL,				// Use parent's starting directory 
-		&si,				// Pointer to STARTUPINFO structure
-		&pi)				// Pointer to PROCESS_INFORMATION structure
-		)
-	{
-		printf("CreateProcess failed (%d).\n", GetLastError());
-		//getchar();
-		return;
-	}
-	//getchar();
 
-	// Wait until child process exits.
-	//WaitForSingleObject(pi.hProcess, INFINITE);
-
-	cout << "test" << endl;
-
-	// Close process and thread handles. 
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
+	std::cout << "Elapsed time: " << duration2/duration << " ms" << std::endl;
+	getchar();
 
 
 }
+
+/*
+//When multiplexing later on, each U32 element in bufArray_B must be deMux in 8 segments of 4-bits each
+void Image::demux_()
+{
+	for (int pixIndex = 0; pixIndex < mRTsequence.mNpixAllFrames; pixIndex++)
+	{
+		unsigned char upscaled = mRTsequence.mUpscaleU8 * mBufArrayB[pixIndex]; //Upscale the buffer from 4-bit to 8-bit
+
+																				//If upscaled overflows
+		if (upscaled > _UI8_MAX)
+			upscaled = _UI8_MAX;
+
+		mImage[pixIndex] = upscaled;
+	}
+}
+*/
